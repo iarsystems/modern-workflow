3fca48e6f5f24d7dc350c2778d6e389374d24d4e
Felipe Torrezan <felipe.torrezan@iar.com>
2025-09-26 09:47:53 +0200

    ci: upgraded workflow
    Signed-off-by: Felipe Torrezan <felipe.torrezan@iar.com>

--- cmake/iar-tools.cmake
+++ cmake/iar-tools.cmake
@@ -9,7 +9,6 @@ set(CMAKE_CONFIGURATION_TYPES Debug Release RelWithDebInfo MinSizeRel)
 # Enable IAR C-STAT Static Analysis
 option(ENABLE_ICSTAT "Enable IAR C-STAT Static Analysis" OFF)
 if(ENABLE_ICSTAT)
-  cmake_minimum_required(VERSION 4.1)
   set(CMAKE_C_ICSTAT "${CMAKE_IAR_CSTAT}")
   set(CMAKE_CXX_ICSTAT "${CMAKE_IAR_CSTAT}")
 endif()
