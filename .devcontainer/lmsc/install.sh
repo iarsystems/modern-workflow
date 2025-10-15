911ebf6fba145262c7672b39539853cbffb44c3f
Felipe Torrezan <felipe.torrezan@iar.com>
2025-10-15 09:50:27 +0200

    Update to LMSC v1.8.1

--- .devcontainer/lmsc/install.sh
+++ .devcontainer/lmsc/install.sh
@@ -1,5 +1,5 @@
 #!/bin/env sh
 
-wget https://netstorage.iar.com/FileStore/STANDARD/001/003/739/iar-lmsc-tools_1.5_amd64.deb
+wget https://netstorage.iar.com/FileStore/STANDARD/001/003/926/iar-lmsc-tools_1.8_amd64.deb
 
-apt install -y ./iar-lmsc-tools_1.5_amd64.deb
+apt install -y ./iar-lmsc-tools_1.8_amd64.deb
