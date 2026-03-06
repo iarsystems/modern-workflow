742ffbdef7d1f49b34b377c64aaa2ad899664db2
david-iar <david.kallberg@iar.com>
2026-03-06 16:19:06 +0100

    update license daemon to 1.11.1

--- .devcontainer/lmsc/install.sh
+++ .devcontainer/lmsc/install.sh
@@ -1,6 +1,6 @@
 #!/bin/env sh
 # Product page: https://updates.iar.com/?product=LMSCDAEMON
 
-wget https://netstorage.iar.com/FileStore/STANDARD/001/003/984/iar-lmsc-tools_1.9_amd64.deb
+wget https://netstorage.iar.com/FileStore/STANDARD/001/004/123/iar-lmsc-tools_1.11_amd64.deb
 
-apt install -y ./iar-lmsc-tools_1.9_amd64.deb
+apt install -y ./iar-lmsc-tools_1.11_amd64.deb
