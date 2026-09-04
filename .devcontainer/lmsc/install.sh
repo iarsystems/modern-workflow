f76c2e523308f6c0f8f27339cccbaf09aa39c9d5
Felipe Torrezan <felipe.torrezan@qt.io>
2026-09-04 15:15:22 +0000

    Update to arm-10.10.2

--- .devcontainer/lmsc/install.sh
+++ .devcontainer/lmsc/install.sh
@@ -1,6 +1,6 @@
 #!/bin/env sh
-# Product page: https://updates.iar.com/?product=LMSCDAEMON
 
-wget https://netstorage.iar.com/FileStore/STANDARD/001/004/123/iar-lmsc-tools_1.11_amd64.deb
+# Get the latest LMSC Daemon from its official product page
+wget -q $(wget -qO- https://updates.iar.com/?product=LMSCDAEMON | grep -Po '(?<=href=")[^"]*(?=")' | grep -i '^https://.*deb')
 
-apt install -y ./iar-lmsc-tools_1.11_amd64.deb
+apt install -y ./iar-lmsc-tools_*_amd64.deb
